5e83df08fc82873c4a98108e863f42a397d17904
infojawwak-source <info.jawwak@gmail.com>
2026-09-09 23:38:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const FX_TIMEOUT_MS = 10_000;
 // التسعير
 const CURRENCY_FEE_PERCENT = 3;
 const PROFIT_MARGIN_PERCENT = 5;
-const MAX_RESULTS = 80;
+const MAX_RESULTS = 35;
 
 // حدود البحث
 const MAX_ADULTS = 9;
